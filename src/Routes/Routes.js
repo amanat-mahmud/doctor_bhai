9aa4fa161d7c755fab113b0246891c49bc572cec
mohammad-ruhullah <c201048@ugrad.iiuc.ac.bd>
2023-05-19 21:57:06 +0600

    routes created

---
+++ src/Routes/Routes.js
@@ -0,0 +1,89 @@
+import { createBrowserRouter } from "react-router-dom";
+import Appointment from "../Components/Appointment.js/Appointment";
+import Login from "../Components/Forms/Login";
+import Signup from "../Components/Forms/Signup";
+
+import Home from "../Components/Home/Home";
+import MyAppointment from "../Components/MyAppointment/MyAppointment";
+import AddDoctor from "../Dashboard/AddDoctor";
+import AllUsers from "../Dashboard/AllUsers";
+import ManageDoctors from "../Dashboard/ManageDoctors";
+import Payment from "../Dashboard/Payment";
+
+import DashBoardLayout from "../Layout/DashBoardLayout";
+import Main from "../Layout/Main";
+import AdminRoute from "./AdminRoute";
+import PrivateRoute from "./PrivateRoute";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/appointment",
+        element: <Appointment></Appointment>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/signup",
+        element: <Signup></Signup>,
+      },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <PrivateRoute>
+        <DashBoardLayout></DashBoardLayout>
+      </PrivateRoute>
+    ),
+    children: [
+      {
+        path: "/dashboard",
+        element: <MyAppointment></MyAppointment>,
+      },
+      {
+        path: "/dashboard/allusers",
+        element: (
+          <AdminRoute>
+            <AllUsers></AllUsers>
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "/dashboard/adddoctor",
+        element: (
+          <AdminRoute>
+            <AddDoctor></AddDoctor>
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "/dashboard/managedoctors",
+        element: (
+          <AdminRoute>
+            <ManageDoctors></ManageDoctors>
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "/dashboard/payment/:id",
+        element: (
+          <AdminRoute>
+            <Payment></Payment>
+          </AdminRoute>
+        ),
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/bookings/${params.id}`),
+      },
+    ],
+  },
+]);
